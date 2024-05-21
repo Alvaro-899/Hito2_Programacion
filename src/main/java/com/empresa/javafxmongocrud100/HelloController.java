51c57d16f1017467b38bfe6ef25c7805a4f8c252
alvaro <alvaro@email.com>
2024-05-21 09:30:26 +0200

    Usuario, contraseña y servidor introducido por usuario

--- src/main/java/com/empresa/javafxmongocrud100/HelloController.java
+++ src/main/java/com/empresa/javafxmongocrud100/HelloController.java
@@ -1,6 +1,5 @@
 package com.empresa.javafxmongocrud100;
 
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -8,13 +7,21 @@ import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import org.bson.Document;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
-    private ListView lv_datos;
+    private ListView<Document> lv_datos;
+    @FXML
+    private TextField tfUser;
+    @FXML
+    private PasswordField pfPass;
+    @FXML
+    private TextField tfServer;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -22,15 +29,28 @@ public class HelloController {
     }
 
     @FXML
-    protected void mostrar(){
-        String url= "mongodb+srv://administrador:Abc123456@cluster0.yb2dxfg.mongodb.net/";
-        MongoClient mongoClient = MongoClients.create(url);
-        MongoDatabase database = mongoClient.getDatabase("actividad17");
-        MongoCollection<Document> collection= database.getCollection("clientes");
+    protected void mostrar() {
+        String username = tfUser.getText();
+        String password = pfPass.getText();
+        String server = tfServer.getText();
+        String url = "mongodb+srv://" + username + ":" + password + "@" + server + ".yb2dxfg.mongodb.net/";
 
+        MongoClient mongoClient;
+        try {
+            mongoClient = MongoClients.create(url);
+        } catch (Exception e) {
+            welcomeText.setText("Connection failed: " + e.getMessage());
+            return;
+        }
+
+        MongoDatabase database = mongoClient.getDatabase("actividad17");
+        MongoCollection<Document> collection = database.getCollection("clientes");
+
+        lv_datos.getItems().clear();
         for (Document doc : collection.find()) {
             lv_datos.getItems().add(doc);
         }
-    }
 
+        welcomeText.setText("Data loaded successfully!");
+    }
 }
